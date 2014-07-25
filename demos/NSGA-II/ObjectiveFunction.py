ad9b3d846cc343257b5980ad1d3f2d276305815c
Emma Sawin <ersawin@gmail.com>
2014-07-25 16:10:41 -0700

    updates ObjectiveFunction.py

--- demos/NSGA-II/ObjectiveFunction.py
+++ demos/NSGA-II/ObjectiveFunction.py
@@ -1,6 +1,15 @@
+from dataset2 import Mnist, Dataset
+from hyperparameter import HyperparametersDBN
+from DBNClassifier import test_DBN, interpretObjectives
+
 def get_objectives(hyperparameters):
-	#this is a stand in, should either run a DBN with the given hyperparameters or estimate the outcome
+#takes a list of 8 hyperparameters and returns the four objectives: pretraining time, post-pretrain error, finetuning time, test score
+	hyper = HyperparametersDBN(learningRate=hyperparameters[1],
+                               numberEpochs=hyperparameters[2],
+                               pretrainingEpochs=hyperparameters[3],
+                               pretrainingLearningRate=hyperparameters[4],
+                               nHidden=[hyperparameters[5], hyperparameters[6], hyperparameters[7]])
+	objectives = test_DBN(Mnist(), hyper)
+	return [interpretObjectives(objectives)]
 	
-	x = hyperparameters[0]
-	y = hyperparameters[1]
-	return [(x + y - 10) ** 2, (x + y - 20) ** 2]
+get_objectives([0.5, 0.1, 4, 1, 0.1, 3000, 3000, 3000])
