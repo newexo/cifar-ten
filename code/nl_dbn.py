e5a824e4336486d6d5aec64c44e0165c7157ff49
dvd <davidvondollen@gmail.com>
2014-09-03 15:21:13 -0700

    load data from dataset

--- code/nl_dbn.py
+++ code/nl_dbn.py
@@ -4,27 +4,16 @@ from sklearn.externals import joblib
 from nolearn.dbn import DBN
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+from datasets import *
 
-def load(name):
-    with open(name, 'rb') as f:
-        return cPickle.load(f)
+datasets = dataset.sharedTrain, dataset.sharedValid, dataset.sharedTest
+train_set_x, train_set_y = dataset.sharedTrain
+valid_set_x, valid_set_y = dataset.sharedValid
+test_set_x, test_set_y = dataset.sharedTest
 
-dataset1 = load('/data/cifar10/cifar-10-batches-py/data_batch_1')
-dataset2 = load('/data/cifar10/cifar-10-batches-py/data_batch_2')
-dataset3 = load('/data/cifar10/cifar-10-batches-py/data_batch_3')
-dataset4 = load('/data/cifar10/cifar-10-batches-py/data_batch_4')
-dataset5 = load('/data/cifar10/cifar-10-batches-py/data_batch_5')
-test_batch = load(/data/cifar10/cifar-10-batches-py/test_batch')
 
-data_train = np.vstack([dataset1['data'], dataset2['data'], dataset3['data'],dataset4['data'],dataset5['data']])
-labels_train = np.hstack([dataset1['labels'], dataset2['labels'],dataset3['labels'],dataset4['labels'],dataset5['labels']])
-
-data_train = data_train.astype('float') / 255.
-labels_train = labels_train
-data_test = test_batch['data'].astype('float') / 255.
-labels_test = np.array(test_batch['labels'])
-n_feat = data_train.shape[1]
-n_targets = labels_train.max() + 1
+n_feat = train_set_x.shape[1]
+n_targets = train_set_y.max() + 1
 
 net = DBN(
     [n_feat, n_feat / 3, n_targets],
@@ -34,11 +23,11 @@ net = DBN(
     learn_rate_minimums=0.005,
     verbose=1,
     )
-net.fit(data_train, labels_train)
+net.fit(train_set_x, train_set_y)
 
 
-expected = labels_test
-predicted = net.predict(data_test)
+expected = test_set_y
+predicted = net.predict(test_set_x)
 
 print "Classification report for classifier %s:\n%s\n" % (
     net, classification_report(expected, predicted))
