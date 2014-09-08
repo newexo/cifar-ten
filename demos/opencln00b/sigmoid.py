29082261e2e4ab46f5f8d7dd9da2c46a8be32e34
gustable <peterson.gus@gmail.com>
2014-09-07 19:19:15 -0700

    Execute a Sigmoid function on an operator  Please enter the commit message for your changes. Lines starting

--- demos/opencln00b/sigmoid.py
+++ demos/opencln00b/sigmoid.py
@@ -5,29 +5,47 @@
 import numpy as np
 import pyopencl as cl
 
-a_np = np.random.rand(50000).astype(np.float32)
-b_np = np.random.rand(50000).astype(np.float32)
+a = np.random.rand(50000).astype(np.float32)
+b = np.random.rand(50000).astype(np.float32)
 
 ctx = cl.create_some_context()
 queue = cl.CommandQueue(ctx)
 
 mf = cl.mem_flags
-a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a_np)
-b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b_np)
+mat = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a)
+target = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b)
+#a = pycl_array.to_device(queue, np.random.rand(50000).astype(np.float32))
+
+
+#__global__ void kApplySigmoid(float* mat, float* target, unsigned int len) {
+#    const unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+#    const unsigned int numThreads = blockDim.x * gridDim.x;
+#
+#    for (unsigned int i = idx; i < len; i += numThreads) {
+#        target[i] = 1 / (1 + __expf(-mat[i]));
+#   }
+#}
+# a_g=mat, b_g=target, res_g=len)
 
 prg = cl.Program(ctx, """
-__kernel void sum(__global const float *a_g, __global const float *b_g, __global float *res_g) {
+__kernel void ApplySigmoid(__global const float *mat, __global const float *target, int len) {
+#	int idx = wkgrpIdx.x * wkgrpDim.x + gidIdx.x;
+#   int numWkItm = wkgrpDim.x * gidDim.x;
+
+#   for (unsigned int i = idx; i < len; i += numThreads) {
+  
   int gid = get_global_id(0);
-  res_g[gid] = a_g[gid] + b_g[gid];
+		target[gid] = 1 / (1 + exp(-mat[gid]));
 }
 """).build()
 
-res_g = cl.Buffer(ctx, mf.WRITE_ONLY, a_np.nbytes)
-prg.sum(queue, a_np.shape, None, a_g, b_g, res_g)
+target = cl.Buffer(ctx, mf.WRITE_ONLY, a.nbytes)
+prg.ApplySigmoid(queue, a.shape, None, mat, target, len)
 
-res_np = np.empty_like(a_np)
-cl.enqueue_copy(queue, res_np, res_g)
+res_np = np.empty_like(a)
+cl.enqueue_copy(queue, res_np, len)
 
 # Check on CPU with Numpy:
-print(res_np - (a_np + b_np))
-print(np.linalg.norm(res_np - (a_np + b_np)))
+# target[i] = 1 / (1 + __expf(-mat[i]))
+print(res_np - (a + b))
+print(np.linalg.norm(res_np - (a + b)))
